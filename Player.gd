effbcca19f3d3892bc565d0afe2c21b4fc144689
Jose <jfmdev@gmail.com>
2021-09-10 19:37:09 -0300

    Add support for touch devices

--- Player.gd
+++ Player.gd
@@ -3,22 +3,48 @@ signal hit
 
 export var speed = 400  # How fast the player will move (pixels/sec).
 var screen_size  # Size of the game window.
+var target = Vector2() # Variable to hold the clicked position.
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	hide()
 
+func start(pos):
+	position = pos
+	target = pos
+	show()
+	$CollisionShape2D.disabled = false
+
+func _input(event):
+	# Change the target whenever a touch event happens.
+	if event is InputEventScreenTouch and event.pressed:
+		target = event.position
+
 func _process(delta):
 	var velocity = Vector2()  # The player's movement vector.
+	
+	# Check for keyboard controls.
+	var keyPressed = false
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
+		keyPressed = true
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
+		keyPressed = true
 	if Input.is_action_pressed("ui_down"):
 		velocity.y += 1
+		keyPressed = true
 	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
-		
+		keyPressed = true
+
+	# Check for touch controls.
+	if keyPressed:
+		target = position
+	elif position.distance_to(target) > 10:
+		# Move towards the target and stop when close.
+		velocity = target - position
+
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite.play()
@@ -36,11 +62,6 @@ func _process(delta):
 	elif velocity.y != 0:
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
-
-func start(pos):
-	position = pos
-	show()
-	$CollisionShape2D.disabled = false
 
 func _on_Player_body_entered(body):
 	hide()  # Player disappears after being hit.
