5e8741795ae5519453ae03244d49db2509114108
Jose <jfmdev@gmail.com>
2021-09-10 17:25:42 -0300

    Add sound effects and configure for start button

--- Main.gd
+++ Main.gd
@@ -7,10 +7,12 @@ func _ready():
 	randomize()
 
 func game_over():
+	$Music.stop()
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
 	get_tree().call_group("mobs", "queue_free")
+	$DeathSound.play()
 
 func new_game():
 	score = 0
@@ -18,6 +20,7 @@ func new_game():
 	$StartTimer.start()
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
+	$Music.play()
 
 func _on_StartTimer_timeout():
 	$MobTimer.start()
